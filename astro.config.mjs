80fedb79cb611b96d050dd4f553e46b387da4c67
rs0805 <harshitaakashyap@gmail.com>
2026-06-05 17:39:19 +0530

    feat: changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 export default defineConfig({
   site: 'https://example.com',
   devToolbar: { enabled: false },
+  integrations: [icon()],
 });
